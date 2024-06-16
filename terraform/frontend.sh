3554dbb68b12e93968445931813b2e54c82f9f2a
NarendraReddy <narendra.nya@gmail.com>
2024-06-16 08:43:05 -0700

    all

---
+++ terraform/frontend.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+component=$1
+environment=$2
+app_version=$3
+
+dnf install ansible -y
+pip3.9 install botocore boto3
+ansible-pull -i localhost, -U https://github.com/NarendraNReddy/expense-ansible-roles-tf_Jenkins.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_version
